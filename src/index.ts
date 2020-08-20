3b0268ccf1181020eb0c541c113311bc35e4eba1
Prashant Singh Pawar <prashantpawar@gmail.com>
2020-08-20 11:23:40 -0400

    Added functioning state using vuex

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import store from './store';
 import 'todomvc-common/base.css';
 import 'todomvc-app-css/index.css';
 
 const app = new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  store
 });
